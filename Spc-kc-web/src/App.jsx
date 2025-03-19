2797ed22b8aa4f501b6146469f59e38df3b982d8
Alan Antony <voa.alan@gmail.com>
2025-03-19 18:10:57 +0530

    about and execom start

--- Spc-kc-web/src/App.jsx
+++ Spc-kc-web/src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Event from'./components/Event/event'
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+import Event from "./components/Event/event";
+import Execom from "./components/Execom/execom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./components/About/about";
 
+function App() {
   return (
-    <>
-    <Event></Event>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <About />
+              <Event />
+            </>
+          }
+        />
+        <Route path="/execom" element={<Execom />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;

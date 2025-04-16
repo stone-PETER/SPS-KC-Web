69680b2300d99f39ca79c77a5989f0ac389f126e
Abhinav K Manoj <abhinavmanoj120@gmail.com>
2025-04-17 00:44:11 +0530

    team

---
+++ Spc-kc-web/src/components/pages/team.jsx
@@ -0,0 +1,55 @@
+import "./team.css"
+import React from "react";
+import Navbar from "../Navbar/navbar";
+
+export default function Team()
+{
+    return (
+        <div className="team">
+            <div className="slt">
+                 <h1>Meet the Team</h1>
+                <div className="types">
+                    <h3>Student Team</h3>
+                    <h3>Profesional body</h3>
+                </div>
+                <div className="members">
+                    <div className="mem">
+                        <img src="./src/assets/img/team/image.png" alt="" />
+                        <h3>Samuel John</h3>
+                        <p>Student Representative</p>
+                    </div>
+                    <div className="mem">
+                        <img src="./src/assets/img/team/image.png" alt="" />
+                        <h3>Samuel John</h3>
+                        <p>Student Representative</p>
+                    </div>
+                    <div className="mem">
+                        <img src="./src/assets/img/team/image.png" alt="" />
+                        <h3>Samuel John</h3>
+                        <p>Student Representative</p>
+                    </div>
+                    <div className="mem">
+                        <img src="./src/assets/img/team/image.png" alt="" />
+                        <h3>Samuel John</h3>
+                        <p>Student Representative</p>
+                    </div>
+                    <div className="mem">
+                        <img src="./src/assets/img/team/image.png" alt="" />
+                        <h3>Samuel John</h3>
+                        <p>Student Representative</p>
+                    </div>
+                    <div className="mem">
+                        <img src="./src/assets/img/team/image.png" alt="" />
+                        <h3>Samuel John</h3>
+                        <p>Student Representative</p>
+                    </div>
+                    <div className="mem">
+                        <img src="./src/assets/img/team/image.png" alt="" />
+                        <h3>Samuel John</h3>
+                        <p>Student Representative</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}

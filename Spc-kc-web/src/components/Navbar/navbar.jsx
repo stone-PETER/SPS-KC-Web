ea7f8c2ee6f5e225170ff3519a157d8a90087d2c
Alan Antony <voa.alan@gmail.com>
2025-05-31 08:48:05 +0530

    nav-fix

--- Spc-kc-web/src/components/Navbar/navbar.jsx
+++ Spc-kc-web/src/components/Navbar/navbar.jsx
@@ -1,24 +1,35 @@
-import "./navbar.css"
-import { Link } from "react-router-dom"
-export default function Navbar(){
-    return(
-        <nav className="navbar">
-        <nav className="navbar">
-            <div className="logo"> <a href="index.html"><img src="./src/assets/img/spslogo.png" alt="" /></a></div>
-            <div className="menu-icon" id="menu-icon">
-                <div className="bar"></div>
-                <div className="bar"></div>               
-                <div className="bar"></div>
-            </div>
-            <ul className="nav-links" id="nav-links">
-                <li><a href="#about">Home</a></li>
-                <li><a href="#about">Present Office Bearers</a></li>
-                <li>
-                    <Link to="/team">Team</Link>
-                </li>
-                <li><a href="#about">Awards</a></li>
-            </ul>
-        </nav>
+import "./navbar.css";
+import { Link } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <nav className="navbar">
+        <div className="logo">
+          {" "}
+          <a href="index.html">
+            <img src="./src/assets/img/spslogo.png" alt="" />
+          </a>
+        </div>
+        <div className="menu-icon" id="menu-icon">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <ul className="nav-links" id="nav-links">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/team">Present Office Bearers</a>
+          </li>
+          <li>
+            <Link to="/team">Team</Link>
+          </li>
+          <li>
+            <a href="/#about">Awards</a>
+          </li>
+        </ul>
+      </nav>
     </nav>
-    )
+  );
 }

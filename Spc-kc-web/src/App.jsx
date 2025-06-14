6d399ddf0936542073329d3984a173030dccafde
Alan Antony <voa.alan@gmail.com>
2025-06-14 14:41:17 +0530

    team22

--- Spc-kc-web/src/App.jsx
+++ Spc-kc-web/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "./components/footer/footer";
 import AllEvents from "./components/Event/allEvents";
 import Newsletter from "./components/Newsletter/newsletter";
 import Team25 from "./components/Execom/team25";
+import Team22 from "./components/Execom/team22";
 function App() {
   return (
     <Router>
@@ -27,6 +28,8 @@ function App() {
         <Route path="/team" element={<Team25 />} />
         <Route path="/execom-23" element={<Team23 />} />
         <Route path="/execom-24" element={<Team24 />} />
+        <Route path="/execom-22" element={<Team22 />} />
+
         <Route path="/events" element={<AllEvents />} />
         <Route path="/newsletter" element={<Newsletter />} />
 
